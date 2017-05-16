2d22b9b399560bf4dacdb3993943b332046705b1
gammacrucisa <pwcse1@gmail.com>
2017-05-16 14:44:10 -0700

    visual stuffs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
 import { Component, Input } from '@angular/core';
+import { trigger, state, animate, style, keyframes, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['../assets/css/style.css'],
+/*     
+  background: -webkit-linear-gradient(to right, #a471a2, #006679);
+  background: linear-gradient(to right, #a471a2, #006679); 
+*/
+  animations: [
+    trigger('bgGradient', [
+        state('start', style({
+            background: "linear-gradient(to right, #a471a2, #006679)"
+        })),
+        state('finish', style({
+            background: "linear-gradient(to right, #a471a2, #006679)"
+        })),
+        transition('start <=> finish', animate('5000ms ease-in'))
+    ])
+  ]
 })
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    BrowserAnimationsModule
   ],
   declarations: [
     AppComponent,
