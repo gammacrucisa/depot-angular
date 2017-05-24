543ab6f2357958da8735fd516746351c06b19aee
gammacrucisa <pwcse1@gmail.com>
2017-05-24 00:40:53 -0700

    backend improvements. clean up code

--- src/app/app.home.text.component.ts
+++ src/app/app.home.text.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, animate, style, keyframes, transition } from '@angular/animations';
+import { trigger, animate, style, keyframes, transition } 
+    from '@angular/animations';
 
 @Component({
     selector: 'app-home-text',
@@ -13,44 +14,16 @@ import { trigger, animate, style, keyframes, transition } from '@angular/animati
                     style({ opacity: 1 }),
                 ]))
             ])
-            
         ])
     ]
 })
+
 export class HomeTextComponent implements OnInit {
     
     text: string = 'start';
     
-    current1: string = 'You';
-    current2: string = 'Us';
-
-    wordsArray1: any = [
-        {
-            'contractor0': [
-                'sales0', 
-                'downloads0', 
-                'leads0' 
-        ]},
-        {
-            'contractor1': [
-                'sales1', 
-                'downloads1', 
-                'leads1' 
-        ]},
-        {
-            'contractor2': [
-                'sales2', 
-                'downloads2', 
-                'leads2' 
-        ]},
-        {
-            'contractor3': [
-                'sales3', 
-                'downloads3', 
-                'leads3' 
-        ]}
-
-    ];
+    current1: string = 'Person';
+    current2: string = 'Help';
     
     wordsArray: any = [
         [
@@ -79,59 +52,18 @@ export class HomeTextComponent implements OnInit {
         ]]
 
     ];
-    
-    words: any = {
-        'contractor0': {
-            'sales': 'sales0', 
-            'downloads': 'downloads0', 
-            'leads': 'leads0' 
-        },
-        'contractor1': {
-            'sales': 'sales1', 
-            'downloads': 'downloads1', 
-            'leads': 'leads1' 
-        },
-        'contractor2': {
-            'sales': 'sales2', 
-            'downloads': 'downloads2', 
-            'leads': 'leads2' 
-        },
-        'contractor3': {
-            'sales': 'sales3', 
-            'downloads': 'downloads3', 
-            'leads': 'leads3' 
-        }
-    };
-
-
 
     ngOnInit() {
-        this.foobaz();
+        this.wordScroll();
     }
 
-    foobaz() {
+    wordScroll() {
         for (let i = 0; i < this.wordsArray.length; i = i + 1) {
             this.current1 = this.wordsArray[i][0];
             
             for (let j = 0; j < this.wordsArray[i][1].length; j = j + 1) {
                 this.current2 = this.wordsArray[i][1][j];
             }
-        
         }
     };
-    
-
-    foobar() { // for iterating a nested dictionary while listing the first dimension properties
-        for (let key of Object.keys(this.words)) {
-            setInterval(() => {
-                this.current1 = key;
-            }, 3000);            
-            for (let subkey of Object.keys(this.words[key])) {
-                setInterval(() => {
-                    this.current2 = this.words[key][subkey];
-                }, 3000);
-            }
-        };
-    }
-
 }
